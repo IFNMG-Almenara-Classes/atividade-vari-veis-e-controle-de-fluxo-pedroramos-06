882f55c808adbf4d8def530e5e4c68e4e4737484
Pedro Pedro Inácio Oliveira Ramos <pior@cf5504e.lab.local>
2025-08-01 21:57:12 -0300

    correçao atv1

--- src/questao02/Principal.java
+++ src/questao02/Principal.java
@@ -1,7 +1,11 @@
 package questao02;
 
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+        
         
     }
 }
